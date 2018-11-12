b553e42eb7fc4c7d77298a7604cb3b41a520aa82
Mirka Pavelkova <miroslava.pavelkova@yahoo.ie>
2018-11-12 14:23:44 +0000

    Added pagination and fixed bug with commas rendering at the top of the table

--- main.js
+++ main.js
@@ -59,6 +59,6 @@ function writeToDocument(url) {
          });
          
         
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");
     });
 }
